5ae890d5ef7389f164be39a1ade4b96c5cad3ddf
AL-FAIZKHAN007 <alfaiz.khan1909@gmail.com>
2025-11-30 22:55:15 +0530

    task2code

--- assignment1Task2.py
+++ assignment1Task2.py
@@ -2,6 +2,6 @@
 firstName = input(("Enter your first name: "))
 lastName = input(("Enter your last name: "))
 
-fullName = (firstName + " " + lastName)
+fullName = (firstName+ " " +lastName)
 
 print(f'\nHello, {fullName}! Welcome to the Python program.')
